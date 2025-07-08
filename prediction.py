32e3b2de990fbd104a6258b43585764e2445b7da
Vasundhara-dev-dev <bosevasundhara9@gmail.com>
2025-07-08 20:12:32 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -7,4 +7,9 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScalar
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_excel(
+df = pd.read_excel("online_retail_II (1).xlsx")
+df.head()
+
+df = df[df['InvoiceNo'].notnull()]
+df = df[~df['InvoiceNo'].astype(str).str.startswith('C')]
+df.dropna(inplace = True)
